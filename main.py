d09b9e268e409348808e0ab5f24172759289c048
chinmaykulkarni8 <chinmaykulkarni8@gmail.com>
2019-11-17 00:39:01 +0530

    Get start and end position

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 import requests
 
+# Constants
 MAZE_MIN_SIZE = 10
 MAZE_MAX_SIZE = 20
 PUZZLE_URL = 'https://api.noopschallenge.com/mazebot/random'
+
+# Strings
 STR_MIN_SIZE = 'minSize'
 STR_MAX_SIZE = 'maxSize'
 STR_MAP = 'map'
+STR_START_POS = 'startingPosition'
+STR_END_POS = 'endingPosition'
 
 params = {STR_MIN_SIZE: MAZE_MIN_SIZE, STR_MAX_SIZE: MAZE_MAX_SIZE}
 
 r = requests.get(PUZZLE_URL, params=params)
 data = r.json()
 maze = data[STR_MAP]
+start_pos = data[STR_START_POS]
+end_pos = data[STR_END_POS]
 
-print(maze)
+print(str(maze) + "\n" + str(start_pos) + "\n" + str(end_pos))
