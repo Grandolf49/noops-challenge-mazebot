9a987e302ff8969a27649c56325e9be5f501a827
chinmaykulkarni8 <chinmaykulkarni8@gmail.com>
2019-11-17 00:34:50 +0530

    Get request successfully getting the maze

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print('Python script that attempts to solve the noops challenge mazesolver')
+import requests
+
+MAZE_MIN_SIZE = 10
+MAZE_MAX_SIZE = 20
+PUZZLE_URL = 'https://api.noopschallenge.com/mazebot/random'
+STR_MIN_SIZE = 'minSize'
+STR_MAX_SIZE = 'maxSize'
+STR_MAP = 'map'
+
+params = {STR_MIN_SIZE: MAZE_MIN_SIZE, STR_MAX_SIZE: MAZE_MAX_SIZE}
+
+r = requests.get(PUZZLE_URL, params=params)
+data = r.json()
+maze = data[STR_MAP]
+
+print(maze)
