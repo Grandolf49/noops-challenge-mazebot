6f406e398a5e75886b37cd3d75905bd3f1013fd5
Grandolf49 <chinmaykulkarni8@gmail.com>
2019-11-20 01:27:46 -0800

    Added Dijkstraj's Algorithm implementation in maze_solver.py

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import maze_api_handler as maze_api
 import constants as con
 import maze_solver as solve
 
-'''
 # Get a random maze puzzle
 maze_data_json = maze_api.get_random_maze()
 print(maze_data_json)
@@ -15,14 +14,28 @@ start_pos = maze_data_json[con.STR_START_POS]
 end_pos = maze_data_json[con.STR_END_POS]
 
 # Get solution to the maze
-answer = solve.solve_maze(maze, start_pos, end_pos)
+'''
+maze = [
+    ['X', ' ', ' ', ' ', ' '],
+    [' ', ' ', 'X', ' ', ' '],
+    [' ', 'A', ' ', 'X', 'X'],
+    [' ', 'X', 'X', 'X', 'B'],
+    [' ', ' ', ' ', ' ', ' ']
+]
+start_pos = [1, 2]
+end_pos = [4, 3]
+'''
+
+# answer_bfs = solve.solve_maze_bfs(maze, start_pos, end_pos)
+answer = solve.solve_maze_dijkstraj(maze, start_pos, end_pos)
+print(answer)
 
 # Post this solution to API
 response = maze_api.post_result_directions(answer, maze_path)
 
 print(response)
-'''
 
+'''
 # Get race puzzle
 puz = maze_api.post_start_race('Grandolf49')
 
@@ -45,5 +58,8 @@ while True:
     # print('Sending Solution')
     puz = maze_api.post_result_directions(answer, maze_path)
     if puz[con.STR_RESULT] == con.STR_FINISHED:
-        print(puz[con.STR_MESSAGE])
+        print(maze_data_json)
+        print(puz)
         break
+
+'''
--- maze_solver.py
+++ maze_solver.py
@@ -2,27 +2,28 @@
 import queue
 import constants as con
 
+# Dijkstraj Constants
+VISITED = 1
+NOT_VISITED = 0
 
-def solve_maze(maze, start_pos, end_pos):
+
+def solve_maze_bfs(maze, start_pos, end_pos):
     # Finding the number of steps
+
     count = 0
     no_of_steps = 0
     maze_len = len(maze)
-    queue_maze = queue.Queue(maze_len ** 5)
+    queue_maze = queue.Queue(maze_len ** 2)
     first_cell = [start_pos[0], start_pos[1], count]
     queue_maze.put(first_cell)
-    flag = 0
-    count1 = 0
+
     while not queue_maze.empty():
         cell = queue_maze.get()
         r = cell[1]
         c = cell[0]
         current_level = cell[2]
-        # print(current_level)
-        count1 += 1
         maze[r][c] = str(current_level)
         if r == end_pos[1] and c == end_pos[0]:
-            flag = 1
             no_of_steps = current_level
             break
         else:
@@ -38,11 +39,7 @@ def solve_maze(maze, start_pos, end_pos):
             if r - 1 > -1 and (maze[r - 1][c] == con.EMPTY or maze[r - 1][c] == con.DESTINATION):
                 next_cell = [c, r - 1, current_level + 1]
                 queue_maze.put(next_cell)
-    # if flag:
-    # print('Shortest Path Found!', 'Steps:', no_of_steps)
-    # else:
-    # print('No path found!')
-    queue_maze.task_done()
+
     path = ''
     current_cell = [end_pos[1], end_pos[0]]
     for i in range(no_of_steps, 0, -1):
@@ -67,4 +64,104 @@ def solve_maze(maze, start_pos, end_pos):
             continue
 
     path = path[::-1]
+    return path
+
+
+def get_min_node(visited, weights, no_of_nodes):
+    min_node = 0
+    min_weight = 1
+    for i in range(no_of_nodes):
+        if visited[i] == NOT_VISITED:
+            min_node = i
+            min_weight = weights[min_node]
+            break
+
+    for i in range(no_of_nodes):
+        if weights[i] < min_weight and visited[i] == NOT_VISITED:
+            min_node = i
+            min_weight = weights[min_node]
+
+    return min_node
+
+
+def get_path(parent, source, destination, size):
+    path = []
+    while destination != parent[destination]:
+        path.append(destination)
+        destination = parent[destination]
+    path.reverse()
+
+    path_dir = ''
+    x = int(source / size)
+    y = source % size
+    current_pos = [x, y]
+
+    for i in range(len(path)):
+        next_pos = [int(path[i] / size), path[i] % size]
+        x_pos = current_pos[0] - next_pos[0]
+        y_pos = current_pos[1] - next_pos[1]
+
+        if x_pos == 1 and y_pos == 0:
+            path_dir += con.NORTH
+        if x_pos == 0 and y_pos == -1:
+            path_dir += con.EAST
+        if x_pos == -1 and y_pos == 0:
+            path_dir += con.SOUTH
+        if x_pos == 0 and y_pos == 1:
+            path_dir += con.WEST
+
+        current_pos[0] = next_pos[0]
+        current_pos[1] = next_pos[1]
+    return path_dir
+
+
+def solve_maze_dijkstraj(maze, start_pos, end_pos):
+    size = len(maze)
+    no_of_nodes = size ** 2
+    all_nodes_visited = no_of_nodes
+
+    source = start_pos[1] * size + start_pos[0]
+    destination = end_pos[1] * size + end_pos[0]
+
+    visited = [NOT_VISITED for x in range(no_of_nodes)]
+    weights = [no_of_nodes for x in range(no_of_nodes)]
+    parent = [-1 for x in range(no_of_nodes)]
+
+    visited[source] = VISITED
+    all_nodes_visited -= 1
+    weights[source] = 0
+    parent[source] = source
+
+    row_vector = [-1, 1, 0, 0]
+    col_vector = [0, 0, 1, -1]
+
+    for i in range(4):
+        x = start_pos[1] + row_vector[i]
+        y = start_pos[0] + col_vector[i]
+
+        if size > x > -1 and size > y > -1:
+            idx = x * size + y
+            cell = maze[x][y]
+            if cell != con.OBSTACLE:
+                weights[idx] = 1
+                parent[idx] = source
+
+    while all_nodes_visited != 0:
+        u = get_min_node(visited, weights, no_of_nodes)
+        visited[u] = VISITED
+        all_nodes_visited -= 1
+
+        for i in range(4):
+            x = int(u / size) + row_vector[i]
+            y = u % size + col_vector[i]
+
+            if size > x > -1 and size > y > -1:
+                v = x * size + y
+                cell = maze[x][y]
+                if cell != con.OBSTACLE:
+                    if weights[u] + 1 < weights[v]:
+                        weights[v] = weights[u] + 1
+                        parent[v] = u
+
+    path = get_path(parent, source, destination, size)
     return path
